c66d940903cbf6a9f932638ddace1e9c26a62d77
D-Mahadeva <mahadevadmahadev78@gmail.com>
2025-09-09 18:43:27 +0530

    dev

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(1, 3), 4)
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main()
